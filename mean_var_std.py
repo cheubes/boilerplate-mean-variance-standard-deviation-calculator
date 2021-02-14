7051af32ace5e109fa6042c2adb40fa61f9c0d39
cheubes <christophe@heubes.com>
2021-02-14 07:03:42 +0100

    Solved.

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,16 @@ import numpy as np
 
 def calculate(list):
 
+  if len(list) != 9:
+    raise ValueError("List must contain nine numbers.")
 
+  matrix = np.array(list).reshape(3,3)
 
-
-    return calculations
+  return {
+    'mean': [np.mean(matrix, 0).tolist(), np.mean(matrix, 1).tolist(), np.mean(matrix).tolist()],
+    'variance': [np.var(matrix, 0).tolist(), np.var(matrix, 1).tolist(), np.var(matrix).tolist()],
+    'standard deviation': [np.std(matrix, 0).tolist(), np.std(matrix, 1).tolist(), np.std(matrix).tolist()],
+    'max': [np.max(matrix, 0).tolist(), np.max(matrix, 1).tolist(), np.max(matrix).tolist()],
+    'min': [np.min(matrix, 0).tolist(), np.min(matrix, 1).tolist(), np.min(matrix).tolist()],
+    'sum': [np.sum(matrix, 0).tolist(), np.sum(matrix, 1).tolist(), np.sum(matrix).tolist()]
+    }
--- test_module.py
+++ test_module.py
@@ -13,7 +13,7 @@ class UnitTests(unittest.TestCase):
         actual = mean_var_std.calculate([9,1,5,3,3,3,2,9,0])
         expected = {'mean': [[4.666666666666667, 4.333333333333333, 2.6666666666666665], [5.0, 3.0, 3.6666666666666665], 3.888888888888889], 'variance': [[9.555555555555555, 11.555555555555557, 4.222222222222222], [10.666666666666666, 0.0, 14.888888888888891], 9.209876543209875], 'standard deviation': [[3.0912061651652345, 3.39934634239519, 2.0548046676563256], [3.265986323710904, 0.0, 3.8586123009300755], 3.0347778408328137], 'max': [[9, 9, 5], [9, 3, 9], 9], 'min': [[2, 1, 0], [1, 3, 0], 0], 'sum': [[14, 13, 8], [15, 9, 11], 35]}
         self.assertAlmostEqual(actual, expected, "Expected different output when calling 'calculate()' with '[9,1,5,3,3,3,2,9,0]'")
-    
+
     def test_calculate_with_few_digits(self):
         self.assertRaisesRegex(ValueError, "List must contain nine numbers.", mean_var_std.calculate, [2,6,2,8,4,0,1,])
 
